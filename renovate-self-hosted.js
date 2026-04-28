13c3c73755994416f44fee75d6a429b0c0a07f8e
JasonLandbridge <jasonlandbridge@protonmail.com>
2026-04-28 15:42:16 +0200

    ci(renovate): add self-hosted scheduled workflow

---
+++ renovate-self-hosted.js
@@ -0,0 +1,7 @@
+module.exports = {
+  platform: 'github',
+  repositories: ['JasonLandbridge/Arch-Linux-AUR-Packages-Updater'],
+  onboarding: false,
+  requireConfig: 'required',
+  branchPrefix: 'renovate-selfhosted/',
+};
