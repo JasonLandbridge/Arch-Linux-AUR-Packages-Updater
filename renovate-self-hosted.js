d5beadfd616add53376dbbcd947326b2589d0bc3
JasonLandbridge <jasonlandbridge@protonmail.com>
2026-04-29 18:21:14 +0200

    ci(renovate): add self-hosted scheduled workflow

--- renovate-self-hosted.js
+++ renovate-self-hosted.js
@@ -4,4 +4,6 @@ module.exports = {
   onboarding: false,
   requireConfig: 'required',
   branchPrefix: 'renovate-selfhosted/',
+  gitAuthor: 'JasonLandbridge <15127381+JasonLandbridge@users.noreply.github.com>',
+  username: 'JasonLandbridge',
 };
